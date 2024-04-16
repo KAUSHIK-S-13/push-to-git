8494c18d9f1ee97509c6069373053a87d6c5dc58
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:50:52 +0530

    Adding src/main/java/com/d2d/dto/DataTypeDTO.java

---
+++ src/main/java/com/d2d/dto/DataTypeDTO.java
@@ -0,0 +1,16 @@
+package com.d2d.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DataTypeDTO {
+
+    private Integer id;
+    private String dataBaseEngineName;
+    private String dataTypeName;
+    private Boolean isLength;
+
+
+}
