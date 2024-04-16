aac326b165ac4b8e31ff0764c16fd50f57e02219
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:50:51 +0530

    Adding src/main/java/com/d2d/dto/DataBaseEngineDTO.java

---
+++ src/main/java/com/d2d/dto/DataBaseEngineDTO.java
@@ -0,0 +1,12 @@
+package com.d2d.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DataBaseEngineDTO {
+
+    private Integer id;
+    private String dataBaseEngineName;
+}
