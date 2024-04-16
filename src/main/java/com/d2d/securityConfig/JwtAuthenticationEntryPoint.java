3a5c399333075ff6d281a0d82d1972c746a7bd0f
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:51:37 +0530

    Adding src/main/java/com/d2d/securityConfig/JwtAuthenticationEntryPoint.java

---
+++ src/main/java/com/d2d/securityConfig/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.d2d.securityConfig;
+
+import org.springframework.stereotype.Component;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+    private static final long serialVersionUID = -521401304750789166L;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+
+}
