acdd7114bebb2bfc3844b4fe2f0687516fa83b36
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:51:35 +0530

    Adding src/main/java/com/d2d/response/CommonConstants.java

---
+++ src/main/java/com/d2d/response/CommonConstants.java
@@ -0,0 +1,12 @@
+package com.d2d.response;
+
+public class CommonConstants {
+    public static final String SAVED_SUCCESSFULLY = "Saved successfully";
+    public static final String UPDATED_SUCCESSFULLY = "Updated successfully";
+    public static final String DELETED_SUCCESSFULLY = "Deleted successfully";
+    public static final String MAIL_TEXT = "D2D Application throwing Exception is, ";
+    public static final String MAIL_SUBJECT = "D2D Application Exception - Regarding";
+
+    private CommonConstants() {
+    }
+}
