4d97d2b178a05e46e8bf4031d3b9b88a2a831af7
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:50:53 +0530

    Adding src/main/java/com/d2d/dto/DataTypeSaveDTO.java

---
+++ src/main/java/com/d2d/dto/DataTypeSaveDTO.java
@@ -0,0 +1,11 @@
+package com.d2d.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DataTypeSaveDTO {
+    private Integer dataBaseEngineId;
+    private String dataTypeName;
+}
