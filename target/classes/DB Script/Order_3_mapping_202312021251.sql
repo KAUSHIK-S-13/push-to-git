0325e1041860814868d1b56b9840490e1dfd2463
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:53:33 +0530

    Adding target/classes/DB Script/Order_3_mapping_202312021251.sql

---
+++ target/classes/DB Script/Order_3_mapping_202312021251.sql
@@ -0,0 +1,3 @@
+INSERT INTO d2d.mapping (mapping_type,is_active,deleted_flag,created_at,updated_at) VALUES
+	 ('OneToOne',1,0,'2023-05-11 05:45:27','2023-05-11 05:48:58'),
+	 ('ManyToOne',1,0,'2023-05-11 05:45:39','2023-05-11 05:45:39');
