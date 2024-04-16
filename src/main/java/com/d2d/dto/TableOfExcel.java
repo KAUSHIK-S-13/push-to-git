3a13d06d1c83115b4fac6bba0f5a226d10f1d3aa
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:51:11 +0530

    Adding src/main/java/com/d2d/dto/TableOfExcel.java

---
+++ src/main/java/com/d2d/dto/TableOfExcel.java
@@ -0,0 +1,37 @@
+package com.d2d.dto;
+
+import java.util.List;
+
+
+public class TableOfExcel {
+
+    private String name;
+
+    private List<Column> columns;
+
+    private String tableComments;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<Column> columns) {
+        this.columns = columns;
+    }
+
+    public String getTableComments() {
+        return tableComments;
+    }
+
+    public void setTableComments(String tableComments) {
+        this.tableComments = tableComments;
+    }
+}
