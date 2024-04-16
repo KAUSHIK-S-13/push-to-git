d4754c373f3f67ef61e862413b221e8a5e604dd5
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:50:37 +0530

    Adding src/main/java/com/d2d/config/EmailSender.java

---
+++ src/main/java/com/d2d/config/EmailSender.java
@@ -0,0 +1,44 @@
+
+package com.d2d.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+@Configuration
+public class EmailSender {
+
+    @Value("${app.mail.username}")
+    private String userName;
+
+    @Value("${app.mail.port}")
+    private int port;
+
+    @Value("${app.mail.password}")
+    private String password;
+
+    @Value("${app.mail.host}")
+    private  String mailHost;
+
+    @Bean
+    public JavaMailSenderImpl mailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(mailHost);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(userName);
+        javaMailSender.setPassword(password);
+        Properties properties = javaMailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.debug","true");
+        return  javaMailSender;
+    }
+
+}
+
