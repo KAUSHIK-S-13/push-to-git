fa30c3ff25f7c5bae1301fae38cabc235ac70a8f
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:51:00 +0530

    Adding src/main/java/com/d2d/dto/MappingDTO.java

---
+++ src/main/java/com/d2d/dto/MappingDTO.java
@@ -0,0 +1,14 @@
+package com.d2d.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class MappingDTO {
+
+    private Integer id;
+
+    private String mappingType;
+}
