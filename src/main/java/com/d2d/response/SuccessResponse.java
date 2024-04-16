1056a4a30e74a3cf636ecc8801e8982c08cd2b7f
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:51:37 +0530

    Adding src/main/java/com/d2d/response/SuccessResponse.java

---
+++ src/main/java/com/d2d/response/SuccessResponse.java
@@ -0,0 +1,33 @@
+package com.d2d.response;
+
+public class SuccessResponse<T>{
+
+
+    private int statusCode = 200;
+    private String statusMessage = "Success";
+    private T data;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
