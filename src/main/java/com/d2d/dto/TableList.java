ecddb1b9c92f077deb6208259d36c1cda4a8b6a3
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:51:10 +0530

    Adding src/main/java/com/d2d/dto/TableList.java

---
+++ src/main/java/com/d2d/dto/TableList.java
@@ -0,0 +1,15 @@
+package com.d2d.dto;
+
+import java.util.List;
+
+public class TableList {
+    private List<TableOfExcel> tableLists;
+
+    public List<TableOfExcel> getTableLists() {
+        return tableLists;
+    }
+
+    public void setTableLists(List<TableOfExcel> tableLists) {
+        this.tableLists = tableLists;
+    }
+}
