a480b8231742baf5a8be6f15fd974d0cc6da9ee3
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:52:01 +0530

    Adding src/main/resources/DB Script/Order_1_data_base_engine_202312021250.sql

---
+++ src/main/resources/DB Script/Order_1_data_base_engine_202312021250.sql
@@ -0,0 +1,6 @@
+INSERT INTO d2d.data_base_engine (data_base_engine_name,is_active,deleted_flag,created_at,updated_at) VALUES
+	 ('MySql',1,0,'2023-04-26 17:57:45','2023-04-26 17:57:45'),
+	 ('PostgreSQL',0,0,'2023-04-27 11:56:52','2023-04-27 11:56:52'),
+	 ('Oracle',1,0,'2023-04-27 08:34:23','2023-04-27 08:34:23'),
+	 ('PostgreSQL',1,0,'2023-05-15 10:19:42','2023-05-15 10:19:42'),
+	 ('Microsoft SQL',1,0,'2023-05-15 12:23:05','2023-05-15 12:23:05');
