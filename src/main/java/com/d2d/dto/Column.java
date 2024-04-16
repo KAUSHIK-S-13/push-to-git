6ef9ebc8abbdf227b1afb1e966c02d609b04f86a
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:50:48 +0530

    Adding src/main/java/com/d2d/dto/Column.java

---
+++ src/main/java/com/d2d/dto/Column.java
@@ -0,0 +1,133 @@
+package com.d2d.dto;
+
+/**
+ * @author Sekhar
+ */
+public class Column {
+
+    private int id;
+    private String name;
+
+    private String datatype;
+
+    private String keytype;
+
+    private String tableReference;
+
+    private String tableReferenceId;
+
+    private String defaultValue;
+
+    private String isNull;
+
+    private String isAutoIncrement;
+
+    private String sampleData;
+    private  String columnComments;
+    private String tableComments;
+    private String mapping;
+
+    public String getMapping() {
+        return mapping;
+    }
+
+    public void setMapping(String mapping) {
+        this.mapping = mapping;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDatatype() {
+        return datatype;
+    }
+
+    public void setDatatype(String datatype) {
+        this.datatype = datatype;
+    }
+
+    public String getKeytype() {
+        return keytype;
+    }
+
+    public void setKeytype(String keytype) {
+        this.keytype = keytype;
+    }
+
+    public String getTableReferenceId() {
+        return tableReferenceId;
+    }
+
+    public void setTableReferenceId(String tableReferenceId) {
+        this.tableReferenceId = tableReferenceId;
+    }
+
+    public String getTableReference() {
+        return tableReference;
+    }
+
+    public void setTableReference(String tableReference) {
+        this.tableReference = tableReference;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getIsNull() {
+        return isNull;
+    }
+
+    public void setIsNull(String isNull) {
+        this.isNull = isNull;
+    }
+
+    public String getIsAutoIncrement() {
+        return isAutoIncrement;
+    }
+
+    public void setIsAutoIncrement(String isAutoIncrement) {
+        this.isAutoIncrement = isAutoIncrement;
+    }
+
+    public String getSampleData() {
+        return sampleData;
+    }
+
+    public void setSampleData(String sampleData) {
+        this.sampleData = sampleData;
+    }
+
+    public String getColumnComments() {
+        return columnComments;
+    }
+
+    public void setColumnComments(String columnComments) {
+        this.columnComments = columnComments;
+    }
+
+    public String getTableComments() {
+        return tableComments;
+    }
+
+    public void setTableComments(String tableComments) {
+        this.tableComments = tableComments;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
