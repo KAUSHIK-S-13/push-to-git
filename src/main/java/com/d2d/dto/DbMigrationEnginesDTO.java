9b0ed3995b382be630e3d8e664c4671205217287
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:50:55 +0530

    Adding src/main/java/com/d2d/dto/DbMigrationEnginesDTO.java

---
+++ src/main/java/com/d2d/dto/DbMigrationEnginesDTO.java
@@ -0,0 +1,23 @@
+package com.d2d.dto;
+
+public class DbMigrationEnginesDTO {
+
+    private String db1;
+    private String db2;
+
+    public String getDb1() {
+        return db1;
+    }
+
+    public void setDb1(String db1) {
+        this.db1 = db1;
+    }
+
+    public String getDb2() {
+        return db2;
+    }
+
+    public void setDb2(String db2) {
+        this.db2 = db2;
+    }
+}
