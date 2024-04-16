ba5e282d6d2095f3ac2eb1cd6e66d876dec64b5b
KAUSHIK-S-13 <82277125+KAUSHIK-S-13@users.noreply.github.com>
2024-04-16 10:51:35 +0530

    Adding src/main/java/com/d2d/response/AuthResponse.java

---
+++ src/main/java/com/d2d/response/AuthResponse.java
@@ -0,0 +1,26 @@
+package com.d2d.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class AuthResponse implements Serializable {
+    public AuthResponse(String token, String refreshToken, String user) {
+        this.token = token;
+        this.user = user;
+        this.refreshToken = refreshToken;
+    }
+
+    private static final long serialVersionUID = 8286210631647330695L;
+
+    private String user;
+
+    private String token;
+
+    private String refreshToken;
+
+    private String data;
+}
